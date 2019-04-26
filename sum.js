787ba25ec06f917e7f22062c1d5f35d39609ee26
unknown <decisive88@gmail.com>
2019-04-26 20:48:28 +0300

    call apply bind

--- sum.js
+++ sum.js
@@ -15,3 +15,19 @@ var logName = function (lang1, lang2) {
 
 var logPersonName = logName.bind(person, 'es');
 logPersonName('en');
+
+logName.call(person, 'en', 'es');
+logName.apply(person, ['en', 'es']);
+
+(function (lang1, lang2) {
+  console.log('Logged: ', this.getFullName());
+  console.log('Arguments: ', lang1 + ' ' + lang2);
+  console.log('----------------------------------');
+}).apply(person, ['en', 'es']);
+
+var person2 = {
+  firstName: 'Jane',
+  lastName: 'Doe'
+};
+
+console.log(person.getFullName.apply(person2));
